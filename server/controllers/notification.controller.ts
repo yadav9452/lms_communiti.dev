d4c49bb1df91796e8595135722a58264bb9f40d7
“yadav9452” <yrahul9452@gmail.com>
2024-06-21 16:19:41 +0530

    feat/ notification deleted using cron after 30 days

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,7 +2,7 @@ import NotificationModel from "../models/notification.model";
 import { NextFunction, Request, Response } from "express";
 import { catchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utlis/errorHandler";
-
+import cron from "node-cron";
 // get all notifications - only for admin
 export const getAllNotificationsAdmin = catchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -47,3 +47,13 @@ export const updateNotificationsAdmin = catchAsyncError(
     }
   }
 );
+
+// delete the notification only admin using node-cron
+cron.schedule("0 0 0 * * *", async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  await NotificationModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thirtyDaysAgo },
+  });
+  console.log("deleted read notifications");
+});
